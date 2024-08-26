5ca302c87004b244b4c50d9b8dbe102e28d252b7
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2024-08-25 22:57:23 -0600

    feat: add related fields to customer
    Fields added:
    - City
    - Department
    
    Also added object of exitent relationships

--- CleanArchitecture.Domain/Entities/Customer.cs
+++ CleanArchitecture.Domain/Entities/Customer.cs
@@ -10,23 +10,35 @@ public partial class Customer : BaseEntity<Guid>
 
 	public required string Address { get; set; }
 
+	public required string IdentificationValue { get; set; }
+
 	public required int GenderId { get; set; }
 
-	public required int NationalityId { get; set; }
+	public int? ProspectId { get; set; }
+
+	public required int CountryId { get; set; }
 
 	public required int IdentificationTypeId { get; set; }
 
-	public required string IdentificationValue { get; set; }
+	public int? NeighborhoodId { get; set; }
 
-	public required int NeighborhoodId { get; set; }
+	public int? CityId { get; set; }
 
-	public int? ProspectId { get; set; }
+	public int? DepartmentId { get; set; }
 
 	public virtual Gender? Gender { get; set; }
 
 	public virtual Prospect? Prospect { get; set; }
 
+	public virtual Country? Country { get; set; }
+
 	public virtual IdentificationType? IdentificationType { get; set; }
+
+	public virtual Neighborhood? Neighborhood { get; set; }
+
+	public virtual City? City { get; set; }
+
+	public virtual Department? Department { get; set; }
 
 	public virtual ICollection<Email> Emails { get; set; } = [];
 
