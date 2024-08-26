229ee55e65906f414b8d78f38d5a7bd7709acf08
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2024-08-25 23:08:29 -0600

    feat: customer entity configuration

--- CleanArchitecture.Infrastructure/Contexts/Configurations/CustomerConfiguration.cs
+++ CleanArchitecture.Infrastructure/Contexts/Configurations/CustomerConfiguration.cs
@@ -6,42 +6,100 @@ namespace Infrastructure.Contexts.Configurations;
 
 internal sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
 {
-	public void Configure(EntityTypeBuilder<Customer> entity)
+	public void Configure(EntityTypeBuilder<Customer> builder)
 	{
-		entity.ToTable(nameof(Customer));
+		builder.HasKey(c => c.Id);
 
-		entity.HasKey(e => e.Id);
+		builder.Property(c => c.Name)
+			.IsRequired()
+			.HasMaxLength(100);
 
-		entity.Property(e => e.Address)
-			.HasMaxLength(100)
-			.IsUnicode(false);
+		builder.Property(c => c.LastName)
+			.IsRequired()
+			.HasMaxLength(100);
 
-		entity.Property(e => e.IdentificationValue)
-			.HasMaxLength(50)
-			.IsUnicode(false);
+		builder.Property(c => c.MarriedLastName)
+			.HasMaxLength(100);
 
-		entity.Property(e => e.LastName)
-			.HasMaxLength(100)
-			.IsUnicode(false);
+		builder.Property(c => c.Address)
+			.IsRequired()
+			.HasMaxLength(250);
 
-		entity.Property(e => e.MarriedLastName)
-			.HasMaxLength(100)
-			.IsUnicode(false);
+		builder.Property(c => c.IdentificationValue)
+			.IsRequired()
+			.HasMaxLength(50);
 
-		entity.Property(e => e.Name)
-			.HasMaxLength(100)
-			.IsUnicode(false);
+		builder.Property(c => c.GenderId)
+			.IsRequired();
 
-		entity.HasOne(d => d.Gender).WithMany(p => p.Customers)
-			.HasForeignKey(d => d.GenderId)
-			.OnDelete(DeleteBehavior.ClientSetNull)
-			.HasConstraintName($"FK_{nameof(Customer)}_{nameof(Gender)}");
+		builder.Property(c => c.ProspectId)
+			.IsRequired(false);
 
-		entity.HasOne(d => d.IdentificationType).WithMany(p => p.Customers)
-			.HasForeignKey(d => d.IdentificationTypeId)
-			.OnDelete(DeleteBehavior.ClientSetNull);
+		builder.Property(c => c.CountryId)
+			.IsRequired();
 
-		entity.HasOne(d => d.Prospect).WithMany(p => p.Customers)
-			.HasForeignKey(d => d.ProspectId);
+		builder.Property(c => c.IdentificationTypeId)
+			.IsRequired();
+
+		builder.Property(c => c.NeighborhoodId)
+			.IsRequired(false);
+
+		builder.Property(c => c.CityId)
+			.IsRequired(false);
+
+		builder.Property(c => c.DepartmentId)
+			.IsRequired(false);
+
+		// Relationships
+		builder.HasOne(c => c.Gender)
+			.WithMany()
+			.HasForeignKey(c => c.GenderId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder.HasOne(c => c.Prospect)
+			.WithMany()
+			.HasForeignKey(c => c.ProspectId)
+			.OnDelete(DeleteBehavior.SetNull);
+
+		builder.HasOne(c => c.Country)
+			.WithMany()
+			.HasForeignKey(c => c.CountryId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder.HasOne(c => c.IdentificationType)
+			.WithMany()
+			.HasForeignKey(c => c.IdentificationTypeId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder.HasOne(c => c.Neighborhood)
+			.WithMany()
+			.HasForeignKey(c => c.NeighborhoodId)
+			.OnDelete(DeleteBehavior.SetNull);
+
+		builder.HasOne(c => c.City)
+			.WithMany()
+			.HasForeignKey(c => c.CityId)
+			.OnDelete(DeleteBehavior.SetNull);
+
+		builder.HasOne(c => c.Department)
+			.WithMany()
+			.HasForeignKey(c => c.DepartmentId)
+			.OnDelete(DeleteBehavior.SetNull);
+
+		// Collections
+		builder.HasMany(c => c.Emails)
+			.WithOne()
+			.HasForeignKey(e => e.CustomerId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder.HasMany(c => c.PhoneNumbers)
+			.WithOne()
+			.HasForeignKey(p => p.CustomerId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder.HasMany(c => c.References)
+			.WithOne()
+			.HasForeignKey(r => r.CustomerId)
+			.OnDelete(DeleteBehavior.Cascade);
 	}
 }
