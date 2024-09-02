574d5ba2b47731563287440d27be8101f749c4a2
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2024-09-01 23:54:19 -0600

    feat: user repository added

---
+++ CleanArchitecture.Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Interfaces;
+
+internal interface IUserRepository : IGenericRepository<User, Guid>
+{
+	/// <summary>
+	/// Gets the user with the specified identifier and its related references eagerly loaded.
+	/// </summary>
+	/// <param name="userId">The unique identifier of the user.</param>
+	/// <returns>The user entity with its related references loaded, or null if not found.</returns>
+	Task<User?> GetByIdWithIncludes(Guid userId);
+}
---
+++ CleanArchitecture.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Infrastructure.Contexts;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+internal class UserRepository(MainContext context) : GenericRepository<User, Guid>(context), IUserRepository
+{
+	public Task<User?> GetByIdWithIncludes(Guid userId)
+	{
+		return GetEntityQuery()
+			.Include(u => u.Gender)
+			.FirstOrDefaultAsync(u => u.Id.Equals(userId));
+	}
+}
