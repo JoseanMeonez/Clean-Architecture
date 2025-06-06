92a60c3ce6d47882c814ffe3a8640ec29abbd4fe
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2025-06-06 00:48:35 -0600

    refactor: using custom mediator in mediatR's references

--- CleanArchitecture.Api/Controllers/CustomerController.cs
+++ CleanArchitecture.Api/Controllers/CustomerController.cs
@@ -1,19 +1,22 @@
 ﻿using Application.Features.Customers.Requests;
+using Application.Mediator;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CustomerController : ControllerBase
+public class CustomerController(IMediator mediator) : ControllerBase
 {
 	// GET: api/<CustomerController>
 	[HttpGet]
-	public async Task<IActionResult> GetAll(GetAllCustomersRequest request) => Ok(await Mediator.Send(request));
+	public async Task<IActionResult> GetAll([FromQuery] GetAllCustomersRequest request)
+		=> Ok(await mediator.Send(request));
 
 	// GET api/<CustomerController>/5
-	[HttpGet("{id:int}")]
-	public async Task<IActionResult> Get(Guid id) => Ok(await Mediator.Send(new GetCustomerByIdRequest(id)));
+	[HttpGet("{id:guid}")]
+	public async Task<IActionResult> Get(Guid id)
+		=> Ok(await mediator.Send(new GetCustomerByIdRequest(id)));
 
 	// POST api/<CustomerController>
 	[HttpPost]
@@ -23,15 +26,15 @@ public class CustomerController : ControllerBase
 	}
 
 	// PUT api/<CustomerController>/5
-	[HttpPut("{id:int}")]
-	public void Put(int id, [FromBody] string value)
+	[HttpPut("{id:guid}")]
+	public void Put(Guid id, [FromBody] string value)
 	{
 		throw new NotImplementedException();
 	}
 
 	// DELETE api/<CustomerController>/5
-	[HttpDelete("{id}")]
-	public void Delete(int id)
+	[HttpDelete("{id:guid}")]
+	public void Delete(Guid id)
 	{
 		throw new NotImplementedException();
 	}
