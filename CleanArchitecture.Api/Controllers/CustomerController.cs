9daddce4baad00d91531ba0c4ab307c27c7fb516
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2025-08-06 22:31:31 -0600

    fix: Update CustomerController to use GUID for IDs
    Changed route parameters and method signatures in CustomerController to use GUIDs instead of integers for customer IDs, ensuring consistency with expected identifier types.

--- CleanArchitecture.Api/Controllers/CustomerController.cs
+++ CleanArchitecture.Api/Controllers/CustomerController.cs
@@ -12,7 +12,7 @@ public class CustomerController : ControllerBase
 	public async Task<IActionResult> GetAll(GetAllCustomersRequest request) => Ok(await Mediator.Send(request));
 
 	// GET api/<CustomerController>/5
-	[HttpGet("{id:int}")]
+	[HttpGet("{id:guid}")]
 	public async Task<IActionResult> Get(Guid id) => Ok(await Mediator.Send(new GetCustomerByIdRequest(id)));
 
 	// POST api/<CustomerController>
@@ -23,15 +23,15 @@ public class CustomerController : ControllerBase
 	}
 
 	// PUT api/<CustomerController>/5
-	[HttpPut("{id:int}")]
-	public void Put(int id, [FromBody] string value)
+	[HttpPut("{id:guid}")]
+	public void Put(Guid id, [FromBody] string value)
 	{
 		throw new NotImplementedException();
 	}
 
 	// DELETE api/<CustomerController>/5
-	[HttpDelete("{id}")]
-	public void Delete(int id)
+	[HttpDelete("{id:guid}")]
+	public void Delete(Guid id)
 	{
 		throw new NotImplementedException();
 	}
